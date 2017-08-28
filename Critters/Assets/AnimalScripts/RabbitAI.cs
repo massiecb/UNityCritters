303d5b09f1612f32afc5c5aa11ad769f775d2904
massiecb <massiec3@mymacewan.ca>
2017-08-27 18:26:01 -0600

    Rabbit  visibility
    Added a material to show rabbits forward direction.
    Added sphere collider and functions for vision, and a raycast to determine LOS.

--- Critters/Assets/AnimalScripts/RabbitAI.cs
+++ Critters/Assets/AnimalScripts/RabbitAI.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RabbitAI : MonoBehaviour {
-
-	// Use this for initialization
+    // Use this for initialization
+    public bool canSeePredator = false;
+    public float RADIUS = 0.5f;
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void OnTriggerEnter (Collider target) {
+        if (target.gameObject.tag == "Predator")
+            canSeePredator = true;
+    }
+
+    private void OnTriggerExit (Collider other) {
+        if (other.gameObject.tag == "Predator")
+            canSeePredator = false;
+    }
+
+    private bool LOSClear (Transform other) {
+        RaycastHit firstTarget;
+        Vector3 delta = transform.position - other.position;
+        if (!Physics.SphereCast(transform.position, RADIUS, transform.forward, out firstTarget, delta.magnitude - RADIUS))
+            return true;
+        return firstTarget.collider.gameObject.tag == other.gameObject.tag;
+    }
 }
